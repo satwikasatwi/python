e9d854b15d0360cf07fdef66d6f4713be38e7a2a
satwikasatwi <110975952+satwikasatwi@users.noreply.github.com>
2022-11-24 18:22:41 +0530

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+if a<b:
+    print('b is bigger')
+    
+if a>b:
+      print('a is bigger')
+if a==b:
+          print('both are equal')
+if  a!=b:
+              print('both are not equal')
+              
