243c56baa78358264cefe1df47c97d5345fd9443
satwikasatwi <110975952+satwikasatwi@users.noreply.github.com>
2022-12-02 22:12:51 +0530

    Add files via upload

---
+++ biggestusefunc.py
@@ -0,0 +1,8 @@
+def biggest():
+ if a>b:
+    print('a is big')
+ else:
+    print('b is big')
+a=int(input())
+b=int(input())
+biggest()
---
+++ eligibleforvote.py
@@ -0,0 +1,6 @@
+age=int(input('age of the voter'))
+gender=input()
+if age>=18:
+    print('yes')
+else:
+    print('no')
